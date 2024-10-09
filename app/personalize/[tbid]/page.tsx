9b1773bebb1aaca9a76f1963ca6bcd6dad5d6a81
xyntechx <xyntechx@gmail.com>
2024-10-08 23:43:28 -0700

    Enable chapter selection

--- app/personalize/[tbid]/page.tsx
+++ app/personalize/[tbid]/page.tsx
@@ -1,7 +1,21 @@
-const Personalize = ({ params }: { params: { tbid: string } }) => {
+import { promises as fs } from "fs";
+import ChapterSelect from "@/app/_components/ChapterSelect";
+
+const Personalize = async ({ params }: { params: { tbid: string } }) => {
+    const file = await fs.readFile(
+        process.cwd() + `/app/_textbooks/${params.tbid}.json`,
+        "utf8"
+    );
+    const textbook = JSON.parse(file);
+    const chapters = [];
+    for (const chap in textbook) {
+        chapters.push(chap.slice(0, chap.length - 3).trim());
+    }
+    chapters.sort((a, b) => Number(a.split(".")[0]) - Number(b.split(".")[0]));
+
     return (
         <main className="w-screen min-h-screen flex items-center justify-center flex-col p-4">
-            <h1>{params.tbid}</h1>
+            <ChapterSelect tbid={params.tbid} chapters={chapters} />
         </main>
     );
 };
