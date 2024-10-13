9b2807ca06e1daf86672a9e7010339b80a4d0125
xyntechx <xyntechx@gmail.com>
2024-10-12 17:10:40 -0700

    Restructure home comps

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import clsx from "clsx";
 
-import BioInput from "./_components/BioInput";
-import { tbList } from "./_utils/tb-list";
+import BioInput from "./BioInput";
+import { tbList } from "../_utils/tb-list";
 
 const Home = () => {
     return (
